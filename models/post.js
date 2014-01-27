ea235efd6c905590df4ee30df600d9becb9a5650
子循 <zixun.cyl@chenyl.local>
2014-01-27 15:54:51 +0800

    add pic and archive

--- models/post.js
+++ models/post.js
@@ -99,6 +99,44 @@ Post.getAll = function(name,callback){
     });
 };
 
+//一次获取十篇文章
+Post.getTen = function(name, page, callback) {
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            var query = {};
+            if (name) {
+                query.name = name;
+            }
+            //使用 count 返回特定查询的文档数 total
+            collection.count(query, function (err, total) {
+                //根据 query 对象查询，并跳过前 (page-1)*10 个结果，返回之后的 10 个结果
+                collection.find(query, {
+                    skip: (page - 1)*10,
+                    limit: 10
+                }).sort({
+                        time: -1
+                    }).toArray(function (err, docs) {
+                        mongodb.close();
+                        if (err) {
+                            return callback(err);
+                        }
+
+                        callback(null, docs, total);
+                    });
+            });
+        });
+    });
+};
+
 /*
 得到一篇文章的信息
  */
@@ -224,7 +262,36 @@ Post.remove = function(name, day, title, callback) {
 };
 
 
-
+//返回所有文章存档信息
+Post.getArchive = function(callback) {
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //返回只包含 name、time、title 属性的文档组成的存档数组
+            collection.find({}, {
+                "name": 1,
+                "time": 1,
+                "title": 1
+            }).sort({
+                    time: -1
+                }).toArray(function (err, docs) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);
+                    }
+                    callback(null, docs);
+                });
+        });
+    });
+};
 
 
 
